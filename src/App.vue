480ddca8722423b16c1fe79bcfb1ecf1aa3c7e98
Quang Anh Ho <quang.anh@readytech.io>
2021-12-22 08:51:06 +0700

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <div class="container">
+      <h1>Todo List Manager</h1>
+      <h6>Power by Vue 3 | rails 6 ...</h6>
+      <Todos />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+  import Todos from "@/components/Todos.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Todos,
   },
 };
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
